9ec4381bb6a9a34a246ec7c8ee37e708e02056b6
rahul1502 <rahulvala1502@gmail.com>
2018-10-17 23:57:08 -0700

    Bigger dataset added

--- linear-regression.py
+++ linear-regression.py
@@ -1,13 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import csv
+
+data = csv.reader(open('data/train.csv', newline=''), delimiter=' ', quotechar='|')
+
+x = np.empty((0,1), float)
+y = np.empty((0,1), float)
 
 
+for row in data:
 
+    x = np.append(x, np.array([[float(row[0].split(',')[0])]]), axis = 0)
+    y = np.append(y, np.array([[float(row[0].split(',')[1])]]), axis = 0)
 
-# training set - force applied
-x = np.array([[4], [5], [7], [9] ,[12], [34], [45], [56], [63], [81], [88] ,[92], [96], [100], [120], [135], [149], [164], [178], [186], [194]])
-# how far the ball goes
-y = np.array([[62], [36], [63], [129], [134], [237], [491], [361], [458], [945], [689], [742], [1020], [890], [1390], [1345], [1578], [1452], [1578],[1987], [1893]])
 
 x_train = np.append(x, np.ones(x.shape), axis = 1)
 
@@ -16,7 +21,7 @@ x_train = np.append(x, np.ones(x.shape), axis = 1)
 # start with the random values of m, class
 n = len(y)
 learning_rate = 0.0001
-epochs = 6
+epochs = 20
 
 theta = np.array([[0.0,0.0]])
 
@@ -29,7 +34,6 @@ for i in range(epochs):
     print('cost: ', cost)
     # regression step
     theta[0] -= (1/len(y)) * learning_rate * np.sum((y_pred - y) * x_train, axis = 0)
-
 
 # Plot
 plt.scatter(x, y)
