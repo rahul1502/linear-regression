9ad18415ce19c81b04206ed9a4e54fb8f9e290be
rahul1502 <rahulvala1502@gmail.com>
2018-10-18 22:40:05 -0700

    Project Complete

--- linear-regression.py
+++ linear-regression.py
@@ -2,43 +2,60 @@ import numpy as np
 import matplotlib.pyplot as plt
 import csv
 
+# get the training data
 data = csv.reader(open('data/train.csv', newline=''), delimiter=' ', quotechar='|')
-
-x = np.empty((0,1), float)
-y = np.empty((0,1), float)
-
-
+x_train = np.empty((0,2), float)
+y_train = np.empty((0,1), float)
 for row in data:
+    x_train = np.append(x_train, np.array([[float(row[0].split(',')[0]), 1.0]]), axis = 0)
+    y_train = np.append(y_train, np.array([[float(row[0].split(',')[1])]]), axis = 0)
+# get the testing data
+data = csv.reader(open('data/test.csv', newline=''), delimiter=' ', quotechar='|')
+x_test = np.empty((0,2), float)
+y_test = np.empty((0,1), float)
+for row in data:
+    x_test = np.append(x_test, np.array([[float(row[0].split(',')[0]),  1.0]]), axis = 0)
+    y_test = np.append(y_test, np.array([[float(row[0].split(',')[1])]]), axis = 0)
 
-    x = np.append(x, np.array([[float(row[0].split(',')[0])]]), axis = 0)
-    y = np.append(y, np.array([[float(row[0].split(',')[1])]]), axis = 0)
-
-
-x_train = np.append(x, np.ones(x.shape), axis = 1)
+# training
+print('Training with ' + str(len(x_train)) + ' tuples')
 
 # y = mx + c
 # parameters = m, c
 # start with the random values of m, class
-n = len(y)
+n = len(y_train)
 learning_rate = 0.0001
 epochs = 20
 
+# theta = [[m, c]]
 theta = np.array([[0.0,0.0]])
 
 # linear regression
 for i in range(epochs):
-
     y_pred = x_train @ theta.T
 
-    cost = np.sum(np.square(y_pred - y)) / (2 * len(y))
-    print('cost: ', cost)
+    error = (1 / (1 * n)) * np.sum(np.square(y_pred - y_train))
+    print('error: ', error)
+
     # regression step
-    theta[0] -= (1/len(y)) * learning_rate * np.sum((y_pred - y) * x_train, axis = 0)
+    # theta is (1,2) matrix so get 0th row using theta[0]
+    theta[0] -= (1/n) * learning_rate * np.sum((y_pred - y_train) * x_train, axis = 0)
 
 # Plot
-plt.scatter(x, y)
-plt.plot(x, y_pred, 'r')
-plt.title('Scatter plot of Prediction')
-plt.xlabel('x')
-plt.ylabel('y')
+plt.scatter(x_train[:,0], y_train)
+plt.plot(x_train[:,0], y_pred, 'r')
+plt.title('Scatter plot of Training')
+plt.xlabel('x_train')
+plt.ylabel('y_train')
+plt.show()
+
+print('---------------------------------------------')
+# testing
+print('Testing with ' + str(len(x_test)) + ' tuples')
+y_test_pred = x_test @ theta.T
+plt.scatter(x_test[:,0], y_test)
+plt.plot(x_train[:,0], y_pred, 'r')
+plt.title('Scatter plot of Testing')
+plt.xlabel('x_test')
+plt.ylabel('y_test')
 plt.show()
